5f212b76d971ae616771f55c77f4c364cd017b25
Joseph Chingalo <profschingalo@gmail.com>
2022-08-11 10:35:56 +0300

    feat: 🎸 intial models and configs for app

---
+++ src/configs/app-config.example.ts
@@ -0,0 +1,7 @@
+import { AppConfigModel } from '../models';
+
+export const appConfig: AppConfigModel = {
+  username: 'dhis_username',
+  password: 'dhis_password',
+  baseUrl: 'dhis_base_url'
+};
---
+++ src/models/index.ts
@@ -0,0 +1,2 @@
+export * from './app-config-model';
+export * from './dhis2-tracked-entry-instance';
