01459b0e52c2ee2b205c0a75d2809d12faaa3582
Joseph Chingalo <profschingalo@gmail.com>
2022-08-12 12:44:25 +0300

    refactor: 💡 TEI getting status

--- src/utils/dhis2-tracked-entity-instance-util.ts
+++ src/utils/dhis2-tracked-entity-instance-util.ts
@@ -18,20 +18,15 @@ export class Dhis2TrackedEntityInstanceUtil {
   }
   ///TODO const url = `${serverUrl}/api/trackedEntityInstances?strategy=CREATE_AND_UPDATE`;
 
-  async discoverTrackedEntityInstances(
+  async discoverTrackedEntityInstancesPageFilters(
     program: string,
-    attributeIds: string[],
-    programStageIds: string[],
-    dataElementIds: string[],
     organisationUniIds: string[] = []
-  ): Promise<Dhis2TrackedEntityInstance[]> {
-    const trackedEntityInstances: any[] = [];
+  ): Promise<string[]> {
     const ouFilter =
       organisationUniIds.length > 0
         ? `ou=${organisationUniIds.join(';')}&ouMode=DESCENDANTS`
         : `ouMode=ACCESSIBLE`;
     const apiUrl = `${this._baseUrl}/api/trackedEntityInstances.json?program=${program}&${ouFilter}`;
-    const fields = `trackedEntityInstance,orgUnit,attributes[attribute,value],enrollments[enrollmentDate,program,events[event,progrogramStage,eventDate,dataValues[value,dataElement]]]`;
     await new LogsUtil().addLogs(
       'info',
       `Discovering pagination details for TEIs : ${program}`,
@@ -41,60 +36,66 @@ export class Dhis2TrackedEntityInstanceUtil {
       this._headers,
       `${apiUrl}&pageSize=1&fields=none&totalPages=true`
     );
-    const pagefilters = AppUtil.getPaginationsFilters(
-      responsePaginations,
-      this.pageSize
-    );
-    let count = 0;
-    for (const pagefilter of _.chunk(pagefilters, 1)[0]) {
-      count++;
-      await new LogsUtil().addLogs(
-        'info',
-        `Discovering details for TEIs : ${program} :::  ${count}/${pagefilters.length}`,
-        'Dhis2 TEI Util'
-      );
-      const url = `${apiUrl}&fields=${fields}&${pagefilter}`;
-      const response: any = await HttpUtil.getHttp(this._headers, url);
-     ;
-     let data : any =  _.map(
-      response.trackedEntityInstances || [],
-      (tei: Dhis2TrackedEntityInstance) => {
-        return {
-          ...tei,
-          attributes: _.filter(tei.attributes || [], (attributObj) =>
-            attributeIds.includes(attributObj.attribute)
-          ),
-          enrollments: _.map(tei.enrollments || [], (enrollment) => {
-            return {
-              ...enrollment,
-              enrollmentDate : AppUtil.getFormattedDate(enrollment.enrollmentDate),
-              events: _.flattenDeep(
-                _.map(
-                  _.filter(enrollment.events || [], (eventObj) =>
-                    programStageIds.includes(eventObj.programStage || '')
-                  ),
-                  (eventObj) => {
-                    return {
-                      ...eventObj,
-                      eventDate : AppUtil.getFormattedDate(eventObj.eventDate),
-                      dataValues: _.filter(
-                        eventObj.dataValues || [],
-                        (dataValue) => {
-                          const id = dataValue.dataElement ?? '';
-                          return dataElementIds.includes(id);
-                        }
-                      )
-                    };
-                  }
+    return AppUtil.getPaginationsFilters(responsePaginations, this.pageSize);
+  }
+
+  async discoverTrackedEntityInstancesByPagination(
+    program: string,
+    attributeIds: string[],
+    programStageIds: string[],
+    dataElementIds: string[],
+    pagefilter: string,
+    organisationUniIds: string[] = []
+  ): Promise<Dhis2TrackedEntityInstance[]> {
+    const fields = `trackedEntityInstance,orgUnit,attributes[attribute,value],enrollments[enrollmentDate,program,events[event,programStage,eventDate,dataValues[value,dataElement]]]`;
+    const ouFilter =
+      organisationUniIds.length > 0
+        ? `ou=${organisationUniIds.join(';')}&ouMode=DESCENDANTS`
+        : `ouMode=ACCESSIBLE`;
+    const apiUrl = `${this._baseUrl}/api/trackedEntityInstances.json?program=${program}&${ouFilter}`;
+    const url = `${apiUrl}&fields=${fields}&${pagefilter}`;
+    const response: any = await HttpUtil.getHttp(this._headers, url);
+    return _.flattenDeep(
+      _.map(
+        response.trackedEntityInstances || [],
+        (tei: Dhis2TrackedEntityInstance) => {
+          return {
+            ...tei,
+            attributes: _.filter(tei.attributes || [], (attributObj) =>
+              attributeIds.includes(attributObj.attribute)
+            ),
+            enrollments: _.map(tei.enrollments || [], (enrollment) => {
+              return {
+                ...enrollment,
+                enrollmentDate: AppUtil.getFormattedDate(
+                  enrollment.enrollmentDate
+                ),
+                events: _.flattenDeep(
+                  _.map(
+                    _.filter(enrollment.events || [], (eventObj) => {
+                      const programStage = eventObj.programStage || '';
+                      return programStageIds.includes(programStage);
+                    }),
+                    (eventObj) => {
+                      return {
+                        ...eventObj,
+                        eventDate: AppUtil.getFormattedDate(eventObj.eventDate),
+                        dataValues: _.filter(
+                          eventObj.dataValues || [],
+                          (dataValue) => {
+                            const id = dataValue.dataElement ?? '';
+                            return dataElementIds.includes(id);
+                          }
+                        )
+                      };
+                    }
+                  )
                 )
-              )
-            };
-          })
-        };
-      }
+              };
+            })
+          };
+        }
+      )
     );
-      trackedEntityInstances.push(data);
-    }
-    return _.flattenDeep(trackedEntityInstances);
   }
 }
