fac2d7104f468d34c05819e5fe8637596cf5643a
Joseph Chingalo <profschingalo@gmail.com>
2022-08-12 12:44:54 +0300

    feat: 🎸 support for getting last event and enrollment dates

--- src/utils/beneficiary-data-util.ts
+++ src/utils/beneficiary-data-util.ts
@@ -1,3 +1,27 @@
+import _ from 'lodash';
+import { Dhis2TrackedEntityInstance } from '../models';
+
 export class BeneficiaryDataUtil {
-  //util functions for beneficairy data;
+  static getLastService(tei: Dhis2TrackedEntityInstance) {
+    const events = _.flattenDeep(
+      _.map(tei.enrollments || [], (enrollment) => enrollment.events || [])
+    );
+    let lastService = {};
+    const sortedServices: any = _.reverse(_.sortBy(events, ['eventDate']));
+    if (sortedServices.length > 0) {
+      lastService = { ...lastService, ...sortedServices[0] };
+    }
+    return lastService;
+  }
+
+  static getCurrentBeneficairyStatus(tei: Dhis2TrackedEntityInstance): string {
+    const lastService = this.getLastService(tei);
+    const enrollmentDate = _.head(
+      _.flattenDeep(
+        _.map(tei.enrollments || [], (enrollment) => enrollment.enrollmentDate)
+      )
+    );
+    console.log({ lastService, enrollmentDate });
+    return 'status';
+  }
 }
