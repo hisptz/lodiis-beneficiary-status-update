554047f5a42d19568a540dfe1f6f95675bbc62ab
Joseph Chingalo <profschingalo@gmail.com>
2022-08-12 11:51:25 +0300

    feat: 🎸 support for get sync status and previous enrollment

--- src/app/beneficiary-data.ts
+++ src/app/beneficiary-data.ts
@@ -1,11 +1,23 @@
+import { BENEFICIARY_STATUS_ID } from '../constants';
 import { Dhis2TrackedEntityInstance } from '../models';
+import { AppUtil } from '../utils';
 
 export class BeneficiaryData {
   private _teiData: Dhis2TrackedEntityInstance;
 
   constructor(tei: Dhis2TrackedEntityInstance) {
     this._teiData = tei;
-    console.log(this._teiData);
+  }
+
+  get shouldSync(): boolean {
+    return this.previousBeneficiaryStatus != this.beneficiaryStatus;
+  }
+
+  get previousBeneficiaryStatus(): string {
+    return AppUtil.getAttributeValueById(
+      this._teiData.attributes || [],
+      BENEFICIARY_STATUS_ID
+    );
   }
 
   get beneficiaryStatus(): string {
